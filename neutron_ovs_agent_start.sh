6172dfb31f48dd547cc52f83c7074fc808160696
Moshe Levi <moshele@nvidia.com>
2020-10-30 10:14:28 +0200

    remove log folder creation as it done by rsync

--- neutron_ovs_agent_start.sh
+++ neutron_ovs_agent_start.sh
@@ -5,11 +5,6 @@ BRANCH=ussuri
 IMAGE_NAME=mellanox/centos-binary-neutron-openvswitch-agent-${BRANCH}-aarch64
 CONTAINER_NAME=neutron_ovs_agent
 
-# Create log folder and grant permissions
-mkdir -p ${LOG_DIR_HOST}
-chown 42435:42435 ${LOG_DIR_HOST}
-chmod 755 ${LOG_DIR_HOST}
-
 # Create container
 docker container create \
 --network host \
